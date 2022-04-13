4cab694fa8852c38868b9576cdb10007c69d447f
Alexandre Genin <alexandre.genin.pro@gmail.com>
2022-04-13 13:09:29 +0200

    updated skelleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
-import Home from "@pages/Home";
-
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <h1>Voici Un Template de base pour démarrer un nouveau Projet React</h1>
+      <p>AXIOS et React-Router-Dom sont déjà installés</p>
+
+      <p>
+        {" "}
+        Il intègre une structure complète et comprend les Dev dépendencies
+        suivantes:
+        <ul>
+          <li>Husky</li>
+          <li>EslInt ( avec une config sur modèle Air BnB)</li>
+          <li>Prettier </li>
+          <li> Vite.js</li>
+        </ul>
+        Attention, si le pluggin Eslint, est activé sur votre CLI il faut le
+        désactiver pour ne pas avoir de conflits avec la dependence installée.
+      </p>
+      <p>Plus de details ,dans les fichiers README</p>
     </div>
   );
 }
